0bd8708df687ab96f0e87ec45baeae79e7c35b56
xeth hung <adamhung12@gmail.com>
2023-04-19 19:55:56 +0800

    fix negative increase

--- src/xh_cpu_usage_simulator/__main__.py
+++ src/xh_cpu_usage_simulator/__main__.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import argparse
+import sys
 from threading import Thread
 
 import psutil
@@ -8,7 +10,33 @@ from xh_cpu_usage_simulator.MovingAvg import MovingAvg
 from xh_cpu_usage_simulator.PsMonitor import PsMonitor
 from xh_cpu_usage_simulator.LinearRegressionPrediction import LinearRegressionPredication
 
+
+def validation(min_usage: float, max_usage: float):
+    if not 0 < min_usage < 100:
+        raise Exception(f"minimum usage[{min_usage}] is out of range")
+    if not 0 < max_usage < 100:
+        raise Exception(f"maximum usage[{max_usage}] is out of range")
+
+    if max_usage < min_usage:
+        raise Exception(f"Usage bound [{min_usage} - {max_usage}] is not valid")
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog="xh-cpu-usage-simulator",
+        usage="A simple tool to simulate system loading of cpu"
+    )
+
+    parser.add_argument("--min", help="minimum percentage cpu usage", type=float)
+    parser.add_argument("--max", help="maximum percentage cpu usage", type=float)
+
+    parsed_args = parser.parse_args(sys.argv[1:])
+
+    lower_bound = parsed_args.min
+    upper_bound = parsed_args.max
+
+    validation(lower_bound, upper_bound)
+
     d = dict()
     r = dict()
     for i in range(psutil.cpu_count(logical=False)):
@@ -21,7 +49,7 @@ if __name__ == '__main__':
 
     m_avg = MovingAvg(10)
 
-    target = 70
+    # target = 70
     rounds = 0
     prediction = LinearRegressionPredication(10)
     for m in PsMonitor().monitor():
@@ -33,26 +61,33 @@ if __name__ == '__main__':
                 cur_total_operation = round(sum([d[i] for i in d]), 2)
                 cur_total_mv = round(sum([r[i] for i in r], 2))
                 prediction.insert(m_avg.avg, cur_total_mv)
-                if prediction.is_ready():
-                    next_operation = prediction.predict(71)
-                else:
-                    next_operation = None
 
-                if target * 0.95 > m_avg.avg:
+                target = round(lower_bound+abs(lower_bound-upper_bound)/2,2)
+
+                def next_operation():
+                    if prediction.is_ready():
+                        return prediction.predict(target)
+                    else:
+                        return None
+
+
+                if lower_bound > m_avg.avg:
                     for i in d:
-                        d[i] = next_operation if next_operation is not None else d[i] * 2
+                        n_op = next_operation()
+                        d[i] = n_op/len(d) if n_op is not None and n_op > 0 else d[i] * 2
                     new_total_operation = round(sum([d[i] for i in d]), 2)
                     print(
-                        f"{rounds:05d}::Targeting[{target}], now[{m_avg.avg}], increase operation with loading[{cur_total_mv}] from {cur_total_operation} to {new_total_operation}")
-                elif target * 1.05 < m_avg.avg:
+                        f"{rounds:05d}::Targeting[{lower_bound} < {target} < {upper_bound}], now[{m_avg.avg}], increase operation with loading[{cur_total_mv}] from {cur_total_operation} to {new_total_operation}")
+                elif upper_bound < m_avg.avg:
                     for i in d:
-                        d[i] = next_operation if next_operation is not None else d[i] * 0.85
+                        n_op = next_operation()
+                        d[i] = n_op/len(d) if n_op is not None else d[i] * 0.85
                     new_total_operation = sum([d[i] for i in d])
                     print(
-                        f"{rounds:05d}Targeting[{target}], now[{m_avg.avg}], decrease operation with loading[{cur_total_mv}] from {cur_total_operation} to {new_total_operation}")
+                        f"{rounds:05d}Targeting[{lower_bound} < {target} < {upper_bound}], now[{m_avg.avg}], decrease operation with loading[{cur_total_mv}] from {cur_total_operation} to {new_total_operation}")
                 else:
                     print(
-                        f"{rounds:05d}Targeting[{target}], now[{m_avg.avg}], nothing to do with loading[{cur_total_mv}]")
+                        f"{rounds:05d}Targeting[{lower_bound} < {target} < {upper_bound}], now[{m_avg.avg}], nothing to do with loading[{cur_total_mv}]")
         m_avg.insert(m.avg)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
